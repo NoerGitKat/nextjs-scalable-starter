eccba1d6b17395ab708a3adeccf8c31a7a33d137
Noer Paanakker <noer@lightworksweb.nl>
2022-08-03 13:20:35 +0200

    build: added storybook

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   // Lint then format TypeScript and JavaScript files
-  "**/*.(ts|tsx|js)": () => [`npx eslint --fix .`, `npx prettier --write .`],
+  "**/*.(ts|tsx|js|jsx)": () => [
+    `npx eslint --fix .`,
+    `npx prettier --write .`,
+    `git add .`,
+  ],
 
   // Format MarkDown and JSON
   "**/*.(md|json)": () => `npx prettier --write .`,
